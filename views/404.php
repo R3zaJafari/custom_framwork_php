dd6b50c4cdab990cbf39a3f33c36d4dd4653bdad
R3zaJafari <reza.jafariiiiiiii8@gmail.com>
2023-03-30 08:18:12 +0430

    delete comment

--- views/404.php
+++ views/404.php
@@ -6,7 +6,7 @@
 
     <main>
         <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-        <!-- Replace with your content -->
+      
         <div class="px-4 py-6 sm:px-0">
             <div class="h-96 rounded-lg border-4 border-dashed border-gray-200"><p>This page doesnt exist</p></div>
         </div>
