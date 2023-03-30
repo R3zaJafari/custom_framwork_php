124a8d74610dd29db07f3f1db0360983e1b20a64
R3zaJafari <reza.jafariiiiiiii8@gmail.com>
2023-03-30 08:20:57 +0430

    again delete

--- views/404.php
+++ views/404.php
@@ -10,7 +10,7 @@
         <div class="px-4 py-6 sm:px-0">
             <div class="h-96 rounded-lg border-4 border-dashed border-gray-200"><p>This page doesnt exist</p></div>
         </div>
-        <!-- /End replace -->
+        
         </div>
     </main>
 
